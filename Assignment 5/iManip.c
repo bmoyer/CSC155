a43c485a458d2057971494e92cca49a3514cc225
Ben Moyer <bmoyer2@pride.hofstra.edu>
2013-10-18 08:37:24 -0400

    Added bitwise operator commands, primitive error catching, string reversal.

---
+++ Assignment 5/iManip.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define chdir _chdir
+
+#else
+#include <unistd.h>
+#endif
+
+#define MAX_LENGTH 1024
+#define DELIMS " \t\r\n"
+
+//these are the functions that handle() will call
+//they handle any commands passed in.
+void reverse(char s[])
+{
+      int length = strlen(s) ;
+      int c, i, j;
+
+      for (i = 0, j = length - 1; i < j; i++, j--)
+     {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+      }
+}
+
+//handles direct print statements
+//another function will handle file commands
+void handle(char *cmdArray[]){
+//printf("arg: %s\n", cmdArray[0]);
+
+if( strcmp(cmdArray[0], "reverse") == 0 && cmdArray[1] != NULL){
+	reverse(cmdArray[1]); printf("%s\n",cmdArray[1]);}		
+if ( strcmp(cmdArray[0], "bit_or") == 0 && cmdArray[1] != NULL)
+	printf("%d\n", atoi(cmdArray[1]) | atoi(cmdArray[2]) );
+if ( strcmp(cmdArray[0], "bit_and") == 0)
+	printf("%d\n", atoi(cmdArray[1]) & atoi(cmdArray[2]) );
+if ( strcmp(cmdArray[0], "bit_xor") == 0)
+	printf("%d\n", atoi(cmdArray[1]) ^ atoi(cmdArray[2]) );
+if ( strcmp(cmdArray[0], "bit_shift_left") == 0)
+	printf("%d\n", atoi(cmdArray[1]) << atoi(cmdArray[2]) );
+if ( strcmp(cmdArray[0], "bit_shift_right") == 0)
+	printf("%d\n", atoi(cmdArray[1]) >> atoi(cmdArray[2]) );
+//else printf("invalid input.");
+}
+
+int main(int argc, char *argv[]) {
+  char *cmd;
+  char line[MAX_LENGTH];
+  char *commandArray[10];
+
+  int i;
+
+  while (1) {
+    printf("$ ");
+    if (!fgets(line, MAX_LENGTH, stdin)) break;
+
+    // Parse and execute command
+    if ((cmd = strtok(line, DELIMS))) {
+	
+	i = 0;	
+	while( cmd != NULL ){
+	
+	commandArray[i] = cmd;	
+//	printf("%s\n", cmd);
+	cmd = strtok(NULL, DELIMS);	
+	i++;
+	}
+	//accessing members of commandArray
+	//printf("arg1: %s arg2: %s", commandArray[0], commandArray[1]);
+	char *firstCmd = commandArray[0];
+
+	//check to make sure command is defined, if not print error message
+	if( strcmp(firstCmd, "reverse") != 0 && strcmp(firstCmd, "bit_or") != 0 && strcmp(firstCmd, "bit_and") != 0 && strcmp(firstCmd, "bit_xor") != 0 && strcmp(firstCmd, "bit_shift_left") != 0 && strcmp(firstCmd, "bit_shift_right") != 0 ) { printf("Improper syntax.\n");}
+	else handle(commandArray);
+	
+	}
+
+	}
+}
+
