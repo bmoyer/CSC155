04d47744f4f73981eb96336e788736153ae86c2a
Ben Moyer <bmoyer2@pride.hofstra.edu>
2013-10-21 22:09:18 -0400

    Realized my program was poorly written and hard to work with.  Completely refactored all code.

--- Assignment 5/iManip.c
+++ Assignment 5/iManip.c
@@ -16,72 +16,89 @@
 
 //these are the functions that handle() will call
 //they handle any commands passed in.
+
+
+//this function should take any char *cmdArray[] and return the correct value
+//this should directly handle int commands, and pass off string commands.
+//ALL COMMANDS SHOULD GO HERe
+
+//this takes cmdArray[] either from a line in a process_file or from user input directly
+char test[100];
+
 void reverse(char s[]){
-      int length = strlen(s) ;
-      int c, i, j;
+int length = strlen(s); int c, i, j;
 
-      for (i = 0, j = length - 1; i < j; i++, j--)
-     {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-      }
-printf("%s\n",s);
+for(i = 0, j = length-1; i<j; i++, j--){
+	c = s[i];
+	s[i] = s[j];
+	s[j] = c;
+	}
+sprintf(test,"%s", s);
 }
 
-void caesar(char x[], int y){
-printf("placeholder");
-}
 
-int handleInt(char *cmdArray[]){
-if ( strcmp(cmdArray[0], "bit_or") == 0 && cmdArray[1] != NULL && cmdArray[2] != NULL)
-	return atoi(cmdArray[1]) | atoi(cmdArray[2]) ;
-if ( strcmp(cmdArray[0], "bit_and") == 0 && cmdArray[1] != NULL && cmdArray[2] != NULL)
-	return atoi(cmdArray[1]) & atoi(cmdArray[2]) ;
-if ( strcmp(cmdArray[0], "bit_xor") == 0 && cmdArray[1] != NULL && cmdArray[2] != NULL)
-	return atoi(cmdArray[1]) ^ atoi(cmdArray[2]);
-if ( strcmp(cmdArray[0], "bit_shift_left") == 0 && cmdArray[1] != NULL && cmdArray[2] != NULL)
-	return atoi(cmdArray[1]) << atoi(cmdArray[2]) ;
-if ( strcmp(cmdArray[0], "bit_shift_right") == 0 && cmdArray[1] != NULL && cmdArray[2] != NULL)
-	return atoi(cmdArray[1]) >> atoi(cmdArray[2]) ;
-}
-
-void handleString(char *cmdArray[])
+char *handle(char *cmdArray[])
 {
-if ( strcmp(cmdArray[0], "caesar_cipher") == 0)
-	caesar(cmdArray[0],atoi(cmdArray[1])); 
-if( strcmp(cmdArray[0], "reverse") == 0 && cmdArray[1] != NULL){
-	reverse(cmdArray[1]);}		
+char *cmd = cmdArray[0];
+if( strcmp(cmd,"bit_or") == 0) { sprintf(test, "%d", atoi(cmdArray[1]) | atoi(cmdArray[2])); return test; }
+if( strcmp(cmd,"bit_and") == 0) { sprintf(test, "%d", atoi(cmdArray[1]) & atoi(cmdArray[2])); return test; }
+if( strcmp(cmd,"bit_xor") == 0) { sprintf(test, "%d", atoi(cmdArray[1]) ^ atoi(cmdArray[2])); return test; }
+if( strcmp(cmd,"bit_shift_right") == 0) { sprintf(test, "%d", atoi(cmdArray[1]) >> atoi(cmdArray[2])); return test; }
+if( strcmp(cmd,"bit_shift_left") == 0) { sprintf(test, "%d", atoi(cmdArray[1]) << atoi(cmdArray[2])); return test; }
+if( strcmp(cmd,"reverse") == 0) { reverse(cmdArray[1]); return test; }
+if( strcmp(cmd,"caesar_cipher") == 0) { return *cmdArray; }//placeholder
 }
 
-void handleProc(char *cmdArray[])
+void processFile(char *cmdArray[])
 {
-FILE *file = fopen(cmdArray[1], "r");
-if (file != NULL){
+FILE *fileIn = fopen(cmdArray[1], "r");
+FILE *fileOut = fopen(cmdArray[2], "wb+");
+char *cmd; int i; char *commandArray[10];
 
+
+if (fileIn != NULL){
 char line[128];
-while( fgets (line, sizeof line, file) != NULL)
+
+while( fgets(line, sizeof line, fileIn) != NULL)
 {
-//	fputs(line, stdout);  //this will print the file line by line
-}
-fclose(file);
-}
-else printf("file was null");
+//this is where you should do stuff line-by-line
+	if (cmd = strtok(line, DELIMS)){
+	i = 0;
+	while( cmd != NULL){
+		commandArray[i] = cmd;
+		cmd = strtok(NULL, DELIMS);
+		i++;
+		}
+	//write results to the new file, plus a newline.
+	fputs( handle(commandArray), fileOut);
+	fputs("\n",fileOut);
+	}
 
-}//end function
+}
+fclose(fileIn);
+fclose(fileOut);
+	}	
+}
 
+void processInput(char *cmdArray[]){
+char *cmd = cmdArray[0];
+if( strcmp(cmd,"process_file") != 0) { printf("%s\n",handle(cmdArray)); }
+if( strcmp(cmd,"process_file") == 0) { processFile(cmdArray);}
+}
+
+
+
+//start of main
 int main(int argc, char *argv[]) {
   char *cmd;
   char line[MAX_LENGTH];
   char *commandArray[10];
-
   int i;
 
   while (1) {
     printf("$ ");
     if (!fgets(line, MAX_LENGTH, stdin)) break;
 
-    // Parse and execute command
     if ((cmd = strtok(line, DELIMS))) {
 	
 	i = 0;	
@@ -90,40 +107,9 @@ int main(int argc, char *argv[]) {
 	cmd = strtok(NULL, DELIMS);	
 	i++;
 	}
+	processInput(commandArray);
 
-	//accessing members of commandArray
-	//printf("arg1: %s arg2: %s", commandArray[0], commandArray[1]);
-	char *firstCmd = commandArray[0];
-
-	//check to make sure command is defined, if not print error message
-
-	//int commands
-	if( strcmp(firstCmd, "bit_or") == 0
-	|| strcmp(firstCmd, "bit_and") == 0 
-	|| strcmp(firstCmd, "bit_xor") == 0 
-	|| strcmp(firstCmd, "bit_shift_left") == 0 
-	|| strcmp(firstCmd, "bit_shift_right") == 0 ) 
-	{ 
-	printf("%d\n", handleInt(commandArray));
-	}
-	
-	//string commands
-	if ( strcmp(firstCmd, "reverse") == 0 
-	|| strcmp(firstCmd, "caesar_cipher") == 0) 
-	{ 
-	handleString(commandArray);
-	}
-	
-	if ( strcmp(firstCmd, "process_file") == 0) {  handleProc(commandArray); }
-	
-	//exit
-	if ( strcmp(firstCmd, "exit") == 0) 
-	{
-	printf("Goodbye.\n");
-	exit(0);
-	}
-	
-}//end command parsing
+}
 
 }
 }
