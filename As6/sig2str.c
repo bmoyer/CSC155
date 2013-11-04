5ec98d36d77d944fddcf533e015a7ce49d7bfddb
Ben Moyer <bmoyer2@pride.hofstra.edu>
2013-11-04 12:17:15 -0500

    Program now outputs signal in plaintext

--- As6/sig2str.c
+++ As6/sig2str.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void sig2str(int signo/*, char *str*/){
+
+char *test = strdup(sys_siglist[signo]);
+printf("%s\n",test);
+}
 
 int main()
 {
 
-
+sig2str(1);
+sig2str(2);
+sig2str(3);
 
 
 }
