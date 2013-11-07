5ab83d9df09f750ac85572cb4e30c076e492e6cd
Ben Moyer <bmoyer2@pride.hofstra.edu>
2013-11-07 10:04:55 -0500

    Got APUE code to compile

---
+++ As6/10.9/prmask.c
@@ -0,0 +1,40 @@
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdint.h>
+void err_sys(const char* x) 
+{ 
+    perror(x); 
+    exit(1); 
+}
+
+void
+pr_mask(const char *str)
+{
+    sigset_t    sigset;
+    int         errno_save;
+
+    errno_save = errno;     /* we can be called by signal handlers */
+    if (sigprocmask(0, NULL, &sigset) < 0)
+        err_sys("sigprocmask error");
+
+    printf("%s", str);
+    if (sigismember(&sigset, SIGINT))   printf("SIGINT ");
+    if (sigismember(&sigset, SIGQUIT))  printf("SIGQUIT ");
+    if (sigismember(&sigset, SIGUSR1))  printf("SIGUSR1 ");
+    if (sigismember(&sigset, SIGALRM))  printf("SIGALRM ");
+
+    /* remaining signals can go here */
+
+    printf("\n");
+    errno = errno_save;
+}
+
+int main()
+{
+//stuff
+}
