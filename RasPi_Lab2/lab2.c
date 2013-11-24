9a2f96099bfabfc753bb1b0e13eb423f4799d6ee
bmoyer <bmoyer2@pride.hofstra.edu>
2013-11-23 19:55:05 -0500

    added functions for processor, kernel, and uptime info

--- RasPi_Lab2/lab2.c
+++ RasPi_Lab2/lab2.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void uptime(){
 
         int i = 0;
         char *token;
         const char DELIMS[2] = ". ";
-        char* timeArray[10];
         char* totalsecs;
         char str[100];
 
@@ -32,5 +31,62 @@ int main(){
 
         //beam it back down
         printf("Uptime: %02d:%02d:%02d:%02d\n", days,hours,minutes,seconds);
+}
+
+void cpuinfo(){
+        int i = 0;
+        char *token;
+        const char DELIMS[2] = ":\n";
+      char* totalsecs;
+        char str[100];
+        char *toks[100];
+
+        //getting contents of /proc/uptime
+        FILE *file = fopen("/proc/cpuinfo","r");
+        fgets(str,100,file);
+        fclose(file);
+
+        int j = 0;
+        //tokenizing stuff
+        token = strtok(str,DELIMS);
+        while(token != NULL){
+        toks[j] = token;
+        token = strtok(NULL, DELIMS);
+        }
+        printf("CPU:%s\n",toks[0]);
+}
+
+void kernelinfo(){
+        int i = 0;
+     char *token;
+        const char DELIMS[1] = "(";
+        char* totalsecs;
+        char str[100];
+        char *toks[100];
+
+        //getting contents of /proc/uptime
+        FILE *file = fopen("/proc/version","r");
+        fgets(str,100,file);
+        fclose(file);
+
+        int j = 1;
+
+        //tokenizing stuff
+        token = strtok(str,DELIMS);
+        toks[0] = token;
+        while(token != NULL){
+        toks[j] = token;
+        token = strtok(NULL, DELIMS);
+        }
+        printf("Kernel:%s\n",toks[0]);
+}
+
+
+int main(){
+
+cpuinfo();
+kernelinfo();
+uptime();
+
 }
 
